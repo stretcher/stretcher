eca9713f2995477f733b23a50c60b74ec11ad02e
Andrew Cholakian <andrew@andrewvc.com>
2013-06-05 07:14:49 -0700

    Cleanup nesting on IndexType specs

--- spec/lib/stretcher_index_type_spec.rb
+++ spec/lib/stretcher_index_type_spec.rb
@@ -69,67 +69,71 @@ describe Stretcher::IndexType do
     end
   end
 
-  describe "put/get/delete/explain" do
+  describe "ops on individual docs" do
     before do
       @doc = {:message => "hello!", :_timestamp => 1366420401}
       @put_res = type.put(987, @doc)
     end
+    
+    describe "put" do
+      it "should put correctly" do
+        @put_res.should_not be_nil
+      end
 
-    it "should put correctly" do
-      @put_res.should_not be_nil
+      it "should post correctly" do
+        type.post(@doc).should_not be_nil
+      end
     end
 
-    it "should post correctly" do
-      type.post(@doc).should_not be_nil
-    end
+    describe "get" do
+      it "should get individual documents correctly" do
+        type.get(987).message.should == @doc[:message]
+      end
 
-    it "should get individual documents correctly" do
-      type.get(987).message.should == @doc[:message]
-    end
+      it "should return nil when retrieving non-extant docs" do
+        lambda {
+          type.get(898323329)
+        }.should raise_exception(Stretcher::RequestError::NotFound)
+      end
 
-    it "should return nil when retrieving non-extant docs" do
-      lambda {
-        type.get(898323329)
-      }.should raise_exception(Stretcher::RequestError::NotFound)
-    end
+      it "should get individual fields given a String, passing through additional options" do
+        res = type.get(987, {:fields => '_timestamp'})
+        res._timestamp.should == @doc[:_timestamp]
+        res.message.should == nil
+      end
 
-    it "should get individual fields given a String, passing through additional options" do
-      res = type.get(987, {:fields => '_timestamp'})
-      res._timestamp.should == @doc[:_timestamp]
-      res.message.should == nil
-    end
+      it "should get individual fields given an Array, passing through additional options" do
+        res = type.get(987, {:fields => ['_timestamp']})
+        res._timestamp.should == @doc[:_timestamp]
+        res.message.should == nil
+      end
 
-    it "should get individual fields given an Array, passing through additional options" do
-      res = type.get(987, {:fields => ['_timestamp']})
-      res._timestamp.should == @doc[:_timestamp]
-      res.message.should == nil
-    end
+      it "should have source and other fields accessible when raw=true" do
+        res = type.get(987, {:fields => ['_timestamp', '_source']}, true)
+        res._source.message == @doc[:message]
+        res.fields._timestamp.should == @doc[:_timestamp]
+      end
 
-    it "should have source and other fields accessible when raw=true" do
-      res = type.get(987, {:fields => ['_timestamp', '_source']}, true)
-      res._source.message == @doc[:message]
-      res.fields._timestamp.should == @doc[:_timestamp]
-    end
+      it "should get individual raw documents correctly" do
+        res = type.get(987, {}, true)
+        res["_id"].should == "987"
+        res["_source"].message.should == @doc[:message]
+      end
 
-    it "should get individual raw documents correctly" do
-      res = type.get(987, {}, true)
-      res["_id"].should == "987"
-      res["_source"].message.should == @doc[:message]
+      it "should get individual raw documents correctly with legacy API" do
+        res = type.get(987, true)
+        res["_id"].should == "987"
+        res["_source"].message.should == @doc[:message]
+      end
     end
-
-    it "should get individual raw documents correctly with legacy API" do
-      res = type.get(987, true)
-      res["_id"].should == "987"
-      res["_source"].message.should == @doc[:message]
-    end
-
+    
     it "should explain a query" do
       type.exists?(987).should be_true
       index.refresh
       res = type.explain(987, {:query => {:match_all => {}}})
       res.should have_key('explanation')
     end
-
+    
     it "should update individual docs correctly" do
       type.update(987, :script => "ctx._source.message = 'Updated!'")
       type.get(987).message.should == 'Updated!'
